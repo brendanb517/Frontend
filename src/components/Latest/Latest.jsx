72ba610dd9dd6db53715e8d1697c3072dcea84d3
Brendan Baird <brendan.b517@gmail.com>
2023-02-15 13:43:35 -0500

    Added latest

--- src/components/Latest/Latest.jsx
+++ src/components/Latest/Latest.jsx
@@ -1,33 +1,26 @@
 // import "./latest.scss";
+// https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=vLVbpyMbdPURogmQJz5HprhlicN7eXZv
 import styled from "styled-components";
+import React, { useState, useEffect } from "react"
 
 const Latest = () => {
-  const latest = [
-    {
-      id: 1,
-      name: "Medium",
-      img: "https://images.pexels.com/photos/159652/table-food-book-newspaper-159652.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      id: 2,
-      name: "The New York Times",
-      img: "https://images.pexels.com/photos/3944377/pexels-photo-3944377.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
-    },
-    {
-      id: 3,
-      name: "Washington Post",
-      img: "https://images.pexels.com/photos/3886870/pexels-photo-3886870.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      id: 4,
-      name: "CoinDesk",
-      img: "https://images.pexels.com/photos/7682249/pexels-photo-7682249.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-  ];
+
+  const [latestNews, setLatestNews ] = useState([])
+  const [newsIndex, setNewsIndex] = useState(0);
+
+  const latest = 
+    useEffect(async function getNews(){
+      await fetch("https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=vLVbpyMbdPURogmQJz5HprhlicN7eXZv")
+      .then(response => response.json())
+      .then(newsData => {
+        setLatestNews(newsData)
+      })}, []);
+    
+      const filteredLatestData = latestNews.slice(newsIndex, newsIndex+4)
 
   return (
     <LatestDiv>
-      {latest.map((news) => {
+      {filteredLatestData.map((news) => {
         return (
           <NewsDiv key={news.id}>
             <img src={news.img} alt="" />
